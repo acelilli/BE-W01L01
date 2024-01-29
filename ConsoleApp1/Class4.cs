afce25d35b13829e81afe7dfffdd4fcde1c11713
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-01-29 16:59:08 +0100

    atleta

---
+++ ConsoleApp1/Class4.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Atleta
+    {
+        private string _nome;
+        public string Nome { get { return _nome; } set { _nome = value; } }
+        public string _cognome;
+        public string Cognome { get { return _cognome; } set { _cognome = value; } }
+        private string _sport;
+        public string Sport { get { return _sport; } set { _sport = value; } }
+        private string _paese;
+        public string Paese { get { return _paese; } set { _paese = value; } }
+
+        public string numMatch(int matchVinti)
+        {
+            if (matchVinti > 10)
+            {
+                return " ed è un grande atleta";
+            }
+            else
+            {
+                return " ma può ancora migliorare";
+            }
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // Righe veicolo
             Veicolo auto = new Veicolo();
             auto.Marca = "Kia";
             Veicolo moto = new Veicolo();
@@ -13,7 +14,7 @@ namespace ConsoleApp1
             Console.WriteLine("Auto è " + auto.Marca);
             Console.WriteLine("Moto è " + moto.Marca);
             Console.WriteLine("\n"); // riga vuota
-            // funzionante! 
+            // Righe animale 
             Animale cane = new Animale();
             cane.Razza = "Yorkshire";
             cane.Nome = "Floki";
@@ -23,11 +24,23 @@ namespace ConsoleApp1
             gatto.Nome = "King";
             Console.WriteLine("Il mio gatto è un " + gatto.Razza + " che si chiama " + gatto.Nome);
             Console.WriteLine("\n"); // riga vuota
+            // Righe dipendente
             Dipendente dipendente1 = new Dipendente();
             dipendente1.Nome = "Tizio";
             dipendente1.Ruolo = "Full Stack Developer";
             dipendente1.Eta = 30;
             Console.WriteLine(dipendente1.Nome + " lavora come " + dipendente1.Ruolo + " e ha " + dipendente1.Eta + " anni.");
+            Console.WriteLine("\n"); // riga vuota
+            // Righe Atleta
+            Atleta atleta1 = new Atleta();
+            atleta1.Nome = "Jannik";
+            atleta1.Cognome = "Sinner";
+            atleta1.Sport = "Tennis";
+            atleta1.Paese = "Italia";
+            Console.WriteLine(atleta1.Nome + atleta1.Cognome + " ha rappresentato " + atleta1.Paese + " nel " + atleta1.Sport);
+            string Messaggio = atleta1.numMatch(13);
+            Console.WriteLine(Messaggio + "\n");
+            Console.WriteLine("\n"); // riga vuota
             Console.ReadLine();
         }
     }
