1435be09cac26e5c83be394a63e24b8b749136e1
Alice Alessandrini <alice.alessandrini.12.12.1993@gmail.com>
2024-01-29 16:11:07 +0100

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Veicolo
+    {
+        private string _marca;
+        public string Marca
+        {
+            get
+            {
+                return _marca;
+            }
+            set
+            {
+                _marca = value;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Veicolo auto = new Veicolo();
+            auto.Marca = "Kia";
+            Veicolo moto = new Veicolo();
+            moto.Marca = "Honda";
+            Console.WriteLine("Auto è " + auto.Marca);
+            Console.WriteLine("\n"); // riga vuota
+            Console.WriteLine("Moto è " + moto.Marca);
+            Console.WriteLine("\n"); // riga vuota
+            // funzionante! 
+            Console.ReadLine();
+        }
+    }
+}
